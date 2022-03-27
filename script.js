6a4700b3f7e234c79400cfee2c05d5f4750e2f14
Valeriy <brdkv19@gmail.com>
2022-03-27 12:27:35 +0500

    css again

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ function loader() {
     var element = document.getElementById("loader");
     element.classList.add("hide");
   }
-setTimeout(function(){loader()}, 250);
+setTimeout(function(){loader()}, 500);
